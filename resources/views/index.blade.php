bcf58969f4be59c8b03618a5f49197798b310017
Mauricio221911210 <al221911210@gmail.com>
2022-04-06 14:34:33 -0500

    diseño

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,18 +3,18 @@
 @section('content')
     <div class="container my-5">
         <h2 class="text-center my-5">Productos</h2>
-        <div class="row">
+        <div class="row  ">
             @foreach ($products as $product)
                 <div class="col-sm-12 col-md-6 col-lg-4">
                     <div class="card">
-                        <img src="{{ $product->get_photo }}" class="card-img-top" alt="...">
-                        <div class="card-body">
+                        <img src="{{ $product->get_photo }}" class="card-img-top  mx-auto" alt="...">
+                        <div class="card-body ">
                             <p class="card-text">{{ $product->name }}</p>
                             <p class="card-text">${{ $product->precio }}</p>
                             <p class="card-text">{{$product->get_status_product}}</p>
                             <form action="{{ route('cart.add', $product) }}" method="post">
                                 @csrf
-                                <button class="btn btn-success">Añadir al carrito</button>
+                                <button class="btn btn-danger">+</button>
                             </form>
                         </div>
                     </div>
